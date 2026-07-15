0e534214eaa8258d6ece08ff5f76053aa00e2d1c
owner <owner@localhost>
2026-07-15 17:03:37 +0900

    tailwind追加

---
+++ rails/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './app/views/**/*.erb', // Rails のビュー
+    './app/helpers/**/*.rb', // ヘルパー内の class も監視
+    './app/javascript/**/*.{js,ts,vue}', // Vite の JS / TS / Vue
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
