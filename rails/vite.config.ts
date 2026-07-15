eb5080a46c930f4b033323c101438675a9226931
owner <owner@localhost>
2026-07-15 16:46:24 +0900

    ホットリロード

--- rails/vite.config.ts
+++ rails/vite.config.ts
@@ -5,4 +5,17 @@ export default defineConfig({
   plugins: [
     RubyPlugin(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 3036,
+    hmr: {
+      host: 'localhost',
+      port: 3036,       // ← 追加
+      clientPort: 3036, // ← 明示的に指定(念のため両方書くと確実)
+    },
+    watch: {
+      usePolling: true,
+      interval: 300,
+    },
+  },
 })
