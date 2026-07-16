b3121a09f082619de7366feb01322129ac0a353f
owner <owner@localhost>
2026-07-16 09:23:35 +0900

    フルリロード追加

--- rails/vite.config.ts
+++ rails/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import FullReload from 'vite-plugin-full-reload'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    FullReload(['config/routes.rb', 'app/views/**/*', 'app/helpers/**/*'], { delay: 200 }),
   ],
   server: {
     host: '0.0.0.0',
