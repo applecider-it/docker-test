6181992efdde55c03bb44c2ff01993a7b18c9eae
owner <owner@localhost>
2026-07-16 18:08:17 +0900

    vue

---
+++ rails/app/javascript/services/development/vue/TestAreaVue.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+/** vueテスト用コンポーネント */
+
+interface Props {
+  testValue?: number;
+}
+
+defineProps<Props>();
+
+const blockStyle = 'border-gray-400 border-2 p-3';
+</script>
+
+<template>
+  <div class="py-6 border-gray-500 border-2 p-5">
+    <div class="mb-6 text-lg">vue.js動作確認</div>
+
+    <div class="space-y-4">
+      <div :class="blockStyle">
+        testValue: {{ testValue }}
+      </div>
+    </div>
+  </div>
+</template>
--- rails/vite.config.ts
+++ rails/vite.config.ts
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
-import FullReload from 'vite-plugin-full-reload'
+import { defineConfig } from 'vite';
+import RubyPlugin from 'vite-plugin-ruby';
+import FullReload from 'vite-plugin-full-reload';
 import path from 'path';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    FullReload(['config/routes.rb', 'app/views/**/*', 'app/helpers/**/*'], { delay: 200 }),
+    vue(),
+    FullReload(['config/routes.rb', 'app/views/**/*', 'app/helpers/**/*'], {
+      delay: 200,
+    }),
   ],
   server: {
     host: '0.0.0.0',
     port: 3036,
     hmr: {
       host: 'localhost',
-      port: 3036,       // ← 追加
+      port: 3036, // ← 追加
       clientPort: 3036, // ← 明示的に指定(念のため両方書くと確実)
     },
     watch: {
@@ -26,4 +30,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'app/javascript'),
     },
   },
-})
+});
