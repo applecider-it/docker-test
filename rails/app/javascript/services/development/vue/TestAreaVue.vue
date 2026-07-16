428d29b3f58ad7d831db12c6ddd03bda2cf07a87
owner <owner@localhost>
2026-07-16 18:14:50 +0900

    vue動作確認

--- rails/app/javascript/services/development/vue/TestAreaVue.vue
+++ rails/app/javascript/services/development/vue/TestAreaVue.vue
@@ -11,7 +11,7 @@ const blockStyle = 'border-gray-400 border-2 p-3';
 </script>
 
 <template>
-  <div class="py-6 border-gray-500 border-2 p-5">
+  <div class="py-6 border-gray-500 border-4 bg-indigo-400 p-5">
     <div class="mb-6 text-lg">vue.js動作確認</div>
 
     <div class="space-y-4">
